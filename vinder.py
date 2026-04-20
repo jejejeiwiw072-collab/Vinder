4e39564ad31ba1a938540275e68d6879b1e987be
jejejeiwiw072-collab <jejejeiwiw072@gmail.com>
2026-04-20 17:17:00 +0700

    Update backend

--- vinder.py
+++ vinder.py
@@ -105,7 +105,10 @@ def get_video_api():
         
         headers = {
             "Content-Disposition": f'attachment; filename="{safe_filename}"',
-            "Content-Type": "video/mp4"
+            "Content-Type": "video/mp4",
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+            "Pragma": "no-cache",
+            "Expires": "0"
         }
         if content_length:
             headers["Content-Length"] = content_length
